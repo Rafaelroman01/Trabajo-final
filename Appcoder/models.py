a46ad4d748e297abf9a9b30ed254bd9fedddcd2c
Rafael Roman <rafaelroman01@gmail.com>
2022-11-30 18:32:54 -0300

    Nuevas Clases

--- Appcoder/models.py
+++ Appcoder/models.py
@@ -4,21 +4,31 @@ from django.db import models
 class Excursion(models.Model):
     nombre = models.CharField(max_length=50)
     email = models.EmailField()
-    def trabajos
+    
+    def __str__(self):
+        return f"Excursion: {self.nombre} | Email: {self.camada}"
     
 class Participantes(models.Model):
     nombre = models.CharField(max_length=50)
     edad = models.IntegerField()
     nacimiento = models.DateField()
     
+    def __str__(self):
+        return f"{self.apellido.upper()}, {self.nombre.capitalize()}"
+    
 class Recreadores(models.Model):
     nombre = models.CharField(max_length=50)
     edad = models.IntegerField()
     nacimiento = models.DateField()
     
+    def __str__(self):
+        return f"{self.apellido.upper()}, {self.nombre.capitalize()} [{self.profesion.title()}]"
     
     
 class Documentacion(models.Model):
     nombre = models.CharField(max_length=50)
     fecha_de_entrega = models.DateField()
     entregado = models.BooleanField()
+    
+    def __str__(self):
+        return f"{self.nombre} -> {self.fecha_de_entrega}"
